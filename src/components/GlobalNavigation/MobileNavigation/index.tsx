4eb900e36f6ad9f92caac20cbe00cee5cd57a93b
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-11-30 18:43:42 +0100

    [AG-019] wip

--- src/components/GlobalNavigation/MobileNavigation/index.tsx
+++ src/components/GlobalNavigation/MobileNavigation/index.tsx
@@ -1,72 +1,77 @@
-"use client";
+'use client'
 
-import Link from "next/link";
+import Link from 'next/link'
 
 type MobileNavigationProps = {
-  isOpen: boolean;
-  onClose: () => void;
-};
+  isOpen: boolean
+  onClose: () => void
+}
 
-const MobileNavigation: React.FC<MobileNavigationProps> = ({
-  isOpen,
-  onClose,
-}) => {
+const MobileNavigation: React.FC<MobileNavigationProps> = ({ isOpen, onClose }) => {
   return (
     <div
       className={[
-        "mobile-navigation",
-        "fixed inset-0",
-        "bg-slate-900 text-white",
-        "pt-[72px] px-6",
-        "transform transition-transform duration-500 ease-out",
-        "md:hidden ",
+        'mobile-navigation',
+        'fixed inset-0',
+        'bg-slate-900 text-white',
+        'px-6 pt-[72px]',
+        'transform transition-transform duration-500 ease-out',
+        'md:hidden',
 
         // MOBILE: from top
         // DESKTOP: from left
-        isOpen ? "translate-y-0" : "-translate-y-full",
+        isOpen ? 'translate-y-0' : '-translate-y-full',
       ]
         .filter(Boolean)
-        .join(" ")}
+        .join(' ')}
     >
-      <nav className="space-y-6 mt-6">
-        <Link
+      <nav className="mt-6 space-y-6">
+        {/* <Link
           href="/en"
-          className="block uppercase tracking-[0.14em] hover:underline"
+          className="block tracking-[0.14em] uppercase hover:underline"
           onClick={onClose}
         >
           Home MOBILE
-        </Link>
-        <Link
-          href="/en/about"
-          className="block uppercase tracking-[0.14em] hover:underline"
-          onClick={onClose}
-        >
-          About
-        </Link>
+        </Link> */}
+
         <Link
           href="/en/rooms"
-          className="block uppercase tracking-[0.14em] hover:underline"
+          className="block tracking-[0.14em] uppercase hover:underline"
           onClick={onClose}
         >
           Rooms
         </Link>
         <Link
-          href="/en/location"
-          className="block uppercase tracking-[0.14em] hover:underline"
+          href="/en/apartments"
+          className="block tracking-[0.14em] uppercase hover:underline"
           onClick={onClose}
         >
-          Location
+          Apartments
+        </Link>
+        <Link
+          href="/en/location"
+          className="block tracking-[0.14em] uppercase hover:underline"
+          onClick={onClose}
+        >
+          Tavernas
+        </Link>
+        <Link
+          href="/en/about"
+          className="block tracking-[0.14em] uppercase hover:underline"
+          onClick={onClose}
+        >
+          About
         </Link>
         <Link
           href="/en/contact"
-          className="block uppercase tracking-[0.14em] hover:underline"
+          className="block tracking-[0.14em] uppercase hover:underline"
           onClick={onClose}
         >
           Contact
         </Link>
       </nav>
     </div>
-  );
-};
+  )
+}
 
-export default MobileNavigation;
+export default MobileNavigation
